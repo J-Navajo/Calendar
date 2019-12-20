ea480389e0d75c333b1169de9adf526e8cde0b1c
J-Navajo <jnnfr.stnly@gmail.com>
2019-12-19 18:05:16 -0700

    My stumble and fall until I spend more time on it.

---
+++ script.js
@@ -0,0 +1,15 @@
+// use moment.js that gets the date
+const date = '2019-03-09';
+const format = 'LLLL';
+const result = moment().format('MMMM Do YYYY, h:mm:ss a');
+// put the date at the top of the page
+
+// add a function that loops through all of the hours
+
+// any hour that is in the past you will use jQuery to change the color
+
+// use text area to be able to type stuff into the hour slots and save into local storage
+
+// when page loads gotta be able to get from local storage and then load on to the page
+
+// 
